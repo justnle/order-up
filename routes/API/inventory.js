a42168a584454c2276834a75ab1d4d6092f6bd9a
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-27 21:32:39 -0700

    Inventory routes

---
+++ routes/API/inventory.js
@@ -0,0 +1,15 @@
+const router = require(`express`).Router();
+const inventoryController = require("../../controllers/inventoryController");
+
+router
+  .route("/")
+  .get(inventoryController.findAll)
+  .post(inventoryController.create);
+
+router
+  .route("/:id")
+  .get(inventoryController.findById)
+  .put(inventoryController.update)
+  .delete(inventoryController.remove);
+
+module.exports = router;
