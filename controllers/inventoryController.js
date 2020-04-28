8b3c9c02c632d387d1a5a8c08fe6779f8c2ed907
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-04-28 00:13:51 -0700

    added use strict to comply with linter rules

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,31 +1,37 @@
+/* eslint-disable object-shorthand */
+/* eslint-disable arrow-parens */
+/* eslint-disable no-magic-numbers */
+
+'use strict';
+
 const db = require(`../models`);
 
 module.exports = {
-    findAll: function (req, res) {
-        db.Inventory.find(req.query)
-          .sort({ date: -1 })
-          .then((dbModel) => res.json(dbModel))
-          .catch((err) => res.status(422).json(err));
-      },
-      findById: function (req, res) {
-        db.Inventory.findById(req.params.id)
-          .then((dbModel) => res.json(dbModel))
-          .catch((err) => res.status(422).json(err));
-      },
-      create: function (req, res) {
-        db.Inventory.create(req.body)
-          .then((dbModel) => res.json(dbModel))
-          .catch((err) => res.status(422).json(err));
-      },
-      update: function (req, res) {
-        db.Inventory.findOneAndUpdate({ _id: req.params.id }, req.body)
-          .then((dbModel) => res.json(dbModel))
-          .catch((err) => res.status(422).json(err));
-      },
-      remove: function (req, res) {
-        db.Inventory.findById({ _id: req.params.id })
-          .then((dbModel) => dbModel.remove())
-          .then((dbModel) => res.json(dbModel))
-          .catch((err) => res.status(422).json(err));
-      },
-    };
+  findAll(req, res) {
+    db.Inventory.find(req.query)
+      .sort({ date: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    db.Inventory.findById(req.params.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.Inventory.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.Inventory.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  remove: function (req, res) {
+    db.Inventory.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  }
+};
