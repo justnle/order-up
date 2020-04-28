b881a3a44da7a9e0177be10e91a40946bd24eac3
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-27 23:56:53 -0700

    API index

---
+++ routes/API/index.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const employeeRoutes = require("./employee");
+const inventoryRoutes = require("./inventory");
+const menuRoutes = require("./menu");
+const timeRoutes = require("./time");
+// Book routes
+router.use("/employee", employeeRoutes);
+router.use("/inventory", inventoryRoutes);
+router.use("./menu", menuRoutes);
+router.use("./time", timeRoutes);
+
+module.exports = router;
