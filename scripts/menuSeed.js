e433325572b95bfa636f6d19f466c1d04f9b7d6e
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-28 23:00:38 -0700

    Fixed menu seed prepare key

--- scripts/menuSeed.js
+++ scripts/menuSeed.js
@@ -13,7 +13,7 @@ const menuSeed = [
     price: 18,
     description: `Beechers Flagship Cheddar, Prosciutto, Grapes, and Grand Central Bakery Multigrain Bread`,
     pairing: `Pinot Grigio`,
-    prepTime: 10,
+    prepareTime: 10,
   },
   {
     category: `Food`,
@@ -21,7 +21,7 @@ const menuSeed = [
     price: 6,
     description: `Shoestring fries seasoned with salt and pepper with a drizzling of cheddar sauce for extra oomph`,
     pairing: `IPA`,
-    prepTime: 10,
+    prepareTime: 10,
   },
   {
     category: `Food`,
@@ -29,7 +29,7 @@ const menuSeed = [
     price: 18,
     description: `Fresh Hama Hama oysters served with sides of cocktail sauce and mignonette`,
     pairing: `Pilsner`,
-    prepTime: 15,
+    prepareTime: 15,
   },
   {
     category: `Food`,
@@ -37,7 +37,7 @@ const menuSeed = [
     price: 12,
     description: `Grilled broccoli, carrots, spinach and feta on toasty naan`,
     pairing: `Pilsner`,
-    prepTime: 20,
+    prepareTime: 20,
   },
   {
     category: `Food`,
@@ -45,7 +45,7 @@ const menuSeed = [
     price: 14,
     description: `1/2 lb burger on toasted Grand Central Bakery Como bread. Toppings include fried onions, mushrooms, and pepperjack cheese.`,
     pairing: `Amber`,
-    prepTime: 20,
+    prepareTime: 20,
   },
   {
     category: `Food`,
@@ -53,7 +53,7 @@ const menuSeed = [
     price: 20,
     description: `Served with charred broccoli and roasted baby red potatoes. `,
     pairing: `Pilsner`,
-    prepTime: 25,
+    prepareTime: 25,
   },
   {
     category: `Food`,
@@ -61,7 +61,7 @@ const menuSeed = [
     price: 30,
     description: `Served with asparagus, basil butter, garlic aioli and hand cut fries`,
     pairing: `Malbec`,
-    prepTime: 25,
+    prepareTime: 25,
   },
   {
     category: `Beverage`,
@@ -69,7 +69,7 @@ const menuSeed = [
     price: 7,
     description: `Dry white wine that has a punchy acidity with flavors of lemons, limes, green apples and honeysuckle`,
     pairing: `Charcuterie`,
-    prepTime: 5,
+    prepareTime: 5,
   },
   {
     category: `Beverage`,
@@ -77,7 +77,7 @@ const menuSeed = [
     price: 7,
     description: `Dark, inky purple color profiles and ripe flavors of plums, black cherry, and blackberry can give this wine a decidedly jammy character`,
     pairing: `NY Strip Steak Frites`,
-    prepTime: 5,
+    prepareTime: 5,
   },
   {
     category: `Beverage`,
@@ -85,7 +85,7 @@ const menuSeed = [
     price: 6,
     description: `Light straw color with spicy floral and strong hop flavors`,
     pairing: `Roasted Lemon Garlic Chicken`,
-    prepTime: 5,
+    prepareTime: 5,
   },
   {
     category: `Beverage`,
@@ -93,7 +93,7 @@ const menuSeed = [
     price: 6,
     description: `Quite malty but not heavily caramelized in flavor`,
     pairing: `The Burger to End All Burgers`,
-    prepTime: 5,
+    prepareTime: 5,
   },
   {
     category: `Beverage`,
@@ -101,7 +101,7 @@ const menuSeed = [
     price: 6,
     description: `Bitter, floral, earthy, citrusy, piney, fruity`,
     pairing: `Hand Cut Fries`,
-    prepTime: 5,
+    prepareTime: 5,
   },
 ];
 db.Menu.remove({})
