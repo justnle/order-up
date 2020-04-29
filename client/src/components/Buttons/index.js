4f42809783c00c98a2838982a6fd1ee7d0f224d2
Teresa Liu <kuas033@gmail.com>
2020-04-28 23:17:37 -0700

    add components

---
+++ client/src/components/Buttons/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+function Buttons() {
+  return (
+    <Button variant="outline-dark">Food</Button>
+
+  );
+}
+
+export default Buttons;
