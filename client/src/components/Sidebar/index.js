8471bf0f48271ac8bdeaa9df3676a094f638c6bc
Teresa Liu <kuas033@gmail.com>
2020-04-28 23:32:05 -0700

    add font awesome to sidebar

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -4,13 +4,33 @@ import Nav from 'react-bootstrap/Nav';
 import './style.css';
 
 function Sidebar() {
+  // these icons are place holders and
+  // do not reflect the actual page
   const navItems = [
-    'Menu',
-    'Inventory',
-    'Employees',
-    'Payroll',
-    'FOH',
-    'BOH',
+    {
+      page: 'Menu',
+      icon: <i class="fas fa-utensils fa-2x"></i>
+    },
+    {
+      page: 'Inventory',
+      icon: <i class="fas fa-cubes fa-2x"></i>
+    },
+    {
+      page: 'Employees',
+      icon: <i class="fas fa-user-friends fa-2x"></i>
+    },
+    {
+      page: 'Payroll',
+      icon: <i class="fas fa-dollar-sign fa-2x"></i>
+    },
+    {
+      page: 'FOH',
+      icon: <i class="fas fa-user-tie fa-2x"></i>
+    },
+    {
+      page: 'BOH',
+      icon: <i class="fas fa-ad fa-2x"></i>
+    }
   ];
 
   return (
@@ -18,9 +38,9 @@ function Sidebar() {
       <div className='sidebar sidebar-sticky bg-dark'>
         <h4 className='px-3'><a href='/home'className='navbar-brand m-0'>Order Up</a></h4>
         <hr className='sidebar-divider'></hr>
-        {navItems.map((page, index) => (
+        {navItems.map((pages, index) => (
           <Nav.Item as='li' key={`list-${index}`}>
-            <Nav.Link href={`/${page.toLowerCase()}`}>{page}</Nav.Link>
+            <Nav.Link href={`/${pages.page.toLowerCase()}`}>{pages.icon} {pages.page}</Nav.Link>
           </Nav.Item>
         ))}
       </div>
