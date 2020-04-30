6824bd34327d30acf2c93fed08a0c939d2047f7a
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-30 13:27:38 -0700

    Menu inline styles swapped for bootstrap styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
-import Menu from "./pages/Menu/index";
 function App() {
   return (
     <div className="App">
       <Sidebar />
-      <Menu />
     </div>
   );
 }
