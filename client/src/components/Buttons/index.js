9362df13fac881cdd3975dc3cb303f9a8a9e0d70
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-29 21:04:20 -0700

    Submit and add buttons

--- client/src/components/Buttons/index.js
+++ client/src/components/Buttons/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Button from "react-bootstrap/Button";
 
-function Buttons() {
+export function AddButton() {
+  return <Button variant="success">Add</Button>;
+}
+export function SubmitButton() {
   return (
-    <Button variant="outline-dark">Food</Button>
-
+    <Button variant="primary" type="Submit">
+      Submit
+    </Button>
   );
 }
 
-export default Buttons;
+// 3. Select button
+// 4. View button
+// 5. Logout button
+// 6. Close button
+// 7. Mark order as sold button
+// 8. Clock in button
+// 9. Clock out button
+// 10. Menu Item button
