656c7f55b7af73e458afdfb229e7b26a051fc6fa
Richard Wang <richardchacha@hotmail.com>
2020-04-29 21:06:22 -0700

    add FOHOrderDisplay

---
+++ client/src/components/FOHOrderDisplay/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+
+function FOHOrderDisplay() {
+  return (
+    <Container>
+      <Card>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Text
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+    </Container>
+  );
+}
+
+export default FOHOrderDisplay;
