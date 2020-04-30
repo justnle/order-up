af3c263d2aef2ff7c2707ea0da738b794ee2e9f3
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-30 00:47:36 -0700

    Search bar rendering and styled

--- client/src/pages/Menu/index.js
+++ client/src/pages/Menu/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import SearchBar from "../../components/SearchBar/index";
+import Container from "react-bootstrap/Container";
+
 class Menu extends React.Component {
   render() {
-    return <SearchBar />;
+    return (
+      <Container style={{ display: "flex", justifyContent: "center" }}>
+        <SearchBar placeholder="Search menu items" style={{ width: "180%" }} />
+      </Container>
+    );
   }
 }
 export default Menu;
