a6054e57aec17c6ab742585d75cb8a0cfdb04a24
Justin <twoplatinumchainz@gmail.com>
2020-04-30 01:43:48 -0700

    Simple BOH interface for the moment

--- client/src/pages/BOH/index.js
+++ client/src/pages/BOH/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import BohHeader from '../../components/BohHeader';
 import BohBody from '../../components/BohBody';
-// import BohTable from '../../components/BohTable';
 
 function Boh() {
   return (
     <Container>
       <BohHeader />
       <BohBody />
-      {/* <BohTable /> */}
     </Container>
   );
 }
