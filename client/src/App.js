ff3c606b875411c1f48183ca5453145954005dfd
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-30 12:10:52 -0700

    Forgot to take the employees out of app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
-import Employees from "./pages/Employees/index";
 function App() {
   return (
     <div className="App">
       <Sidebar />
-      <Employees />
     </div>
   );
 }
