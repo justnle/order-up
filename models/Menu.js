c0d0cba9df85b7a20d4895906c13a9f2830522ed
Richard Wang <richardchacha@hotmail.com>
2020-04-30 20:01:33 -0700

    remove extra commas

--- models/Menu.js
+++ models/Menu.js
@@ -7,32 +7,32 @@ const Schema = mongoose.Schema;
 const MenuSchema = new Schema({
   category: {
     type: String,
-    required: true,
+    required: true
   },
   name: {
     type: String,
-    required: true,
+    required: true
   },
   price: {
     type: Number,
-    required: true,
+    required: true
   },
   description: {
     type: String,
-    required: true,
+    required: true
   },
   pairing: {
     type: String,
-    required: true,
+    required: true
   },
   prepareTime: {
     type: Number,
-    required: true,
+    required: true
   },
   itemCount: {
     type: Number,
-    default: null,
-  },
+    default: null
+  }
 });
 
 module.exports = mongoose.model(`Menu`, MenuSchema);
