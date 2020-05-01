d0f899dec755104760bb2e2484779b763bf086f2
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-04-30 18:25:20 -0700

    created front-end api routes for getting, adding, updating and deleting archived orders

--- client/src/utils/timeAPI.js
+++ client/src/utils/timeAPI.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
 
 export default {
-    // get all time clock information
-    getTimeClock: function () {
-        return axios.get(`/api/time`);
-    },
-    // get individual employee's time clock information
-    getEmployeeTimeClock: function (id) {
-        return axios.get(`/api/time/${ id}`);
-    },
-    // update individual employee's time clock
-    updateEmployeeTimeClock: function (id, data) {
-        return axios.put(`/api/time/${ id}`, data);
-    }  
+  // get all time clock information
+  getTimeClock: function () {
+    return axios.get(`/api/time`);
+  },
+  // get individual employee's time clock information
+  getEmployeeTimeClock: function (id) {
+    return axios.get(`/api/time/${id}`);
+  },
+  // add time clock information
+  addEmployeeTimeClock: function (timeData) {
+    return axios.post(`/api/time`, timeData);
+  },
+  // update individual employee's time clock
+  updateEmployeeTimeClock: function (id, data) {
+    return axios.put(`/api/time/${id}`, data);
+  }
 };
