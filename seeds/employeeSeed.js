5e2c3f52314a6770fb89f9c2313159c9f891b165
Richard Wang <richardchacha@hotmail.com>
2020-04-30 20:17:41 -0700

    update seedDB.js

--- seeds/employeeSeed.js
+++ seeds/employeeSeed.js
@@ -1,13 +1,6 @@
 /* eslint-disable max-len */
 'use strict';
 
-const mongoose = require(`mongoose`);
-const db = require(`../models`);
-
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/orderup`, {
-  useNewUrlParser: true
-});
-
 const employeeSeed = [
   {
     id: 123456,
@@ -71,14 +64,6 @@ const employeeSeed = [
   }
 ];
 
-db.Employees.remove({})
-  .then(() => db.Employees.collection.insertMany(employeeSeed))
-  .then(data => {
-    console.log(`${data.result.n } records inserted!`);
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
-
+module.exports = {
+  employeeSeed
+};
--- seeds/menuSeed.js
+++ seeds/menuSeed.js
@@ -1,11 +1,6 @@
+/* eslint-disable max-len */
 "use strict";
 
-const mongoose = require(`mongoose`);
-const db = require(`../models`);
-
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/orderup`, {
-  useNewUrlParser: true,
-});
 const menuSeed = [
   {
     category: `Food`,
@@ -13,7 +8,7 @@ const menuSeed = [
     price: 18,
     description: `Beechers Flagship Cheddar, Prosciutto, Grapes, and Grand Central Bakery Multigrain Bread`,
     pairing: `Pinot Grigio`,
-    prepareTime: 10,
+    prepareTime: 10
   },
   {
     category: `Food`,
@@ -21,7 +16,7 @@ const menuSeed = [
     price: 6,
     description: `Shoestring fries seasoned with salt and pepper with a drizzling of cheddar sauce for extra oomph`,
     pairing: `IPA`,
-    prepareTime: 10,
+    prepareTime: 10
   },
   {
     category: `Food`,
@@ -29,7 +24,7 @@ const menuSeed = [
     price: 18,
     description: `Fresh Hama Hama oysters served with sides of cocktail sauce and mignonette`,
     pairing: `Pilsner`,
-    prepareTime: 15,
+    prepareTime: 15
   },
   {
     category: `Food`,
@@ -37,7 +32,7 @@ const menuSeed = [
     price: 12,
     description: `Grilled broccoli, carrots, spinach and feta on toasty naan`,
     pairing: `Pilsner`,
-    prepareTime: 20,
+    prepareTime: 20
   },
   {
     category: `Food`,
@@ -45,7 +40,7 @@ const menuSeed = [
     price: 14,
     description: `1/2 lb burger on toasted Grand Central Bakery Como bread. Toppings include fried onions, mushrooms, and pepperjack cheese.`,
     pairing: `Amber`,
-    prepareTime: 20,
+    prepareTime: 20
   },
   {
     category: `Food`,
@@ -53,7 +48,7 @@ const menuSeed = [
     price: 20,
     description: `Served with charred broccoli and roasted baby red potatoes. `,
     pairing: `Pilsner`,
-    prepareTime: 25,
+    prepareTime: 25
   },
   {
     category: `Food`,
@@ -61,7 +56,7 @@ const menuSeed = [
     price: 30,
     description: `Served with asparagus, basil butter, garlic aioli and hand cut fries`,
     pairing: `Malbec`,
-    prepareTime: 25,
+    prepareTime: 25
   },
   {
     category: `Beverage`,
@@ -69,7 +64,7 @@ const menuSeed = [
     price: 7,
     description: `Dry white wine that has a punchy acidity with flavors of lemons, limes, green apples and honeysuckle`,
     pairing: `Charcuterie`,
-    prepareTime: 5,
+    prepareTime: 5
   },
   {
     category: `Beverage`,
@@ -77,7 +72,7 @@ const menuSeed = [
     price: 7,
     description: `Dark, inky purple color profiles and ripe flavors of plums, black cherry, and blackberry can give this wine a decidedly jammy character`,
     pairing: `NY Strip Steak Frites`,
-    prepareTime: 5,
+    prepareTime: 5
   },
   {
     category: `Beverage`,
@@ -85,7 +80,7 @@ const menuSeed = [
     price: 6,
     description: `Light straw color with spicy floral and strong hop flavors`,
     pairing: `Roasted Lemon Garlic Chicken`,
-    prepareTime: 5,
+    prepareTime: 5
   },
   {
     category: `Beverage`,
@@ -93,7 +88,7 @@ const menuSeed = [
     price: 6,
     description: `Quite malty but not heavily caramelized in flavor`,
     pairing: `The Burger to End All Burgers`,
-    prepareTime: 5,
+    prepareTime: 5
   },
   {
     category: `Beverage`,
@@ -101,16 +96,10 @@ const menuSeed = [
     price: 6,
     description: `Bitter, floral, earthy, citrusy, piney, fruity`,
     pairing: `Hand Cut Fries`,
-    prepareTime: 5,
-  },
+    prepareTime: 5
+  }
 ];
-db.Menu.remove({})
-  .then(() => db.Inventory.collection.insertMany(menuSeed))
-  .then((data) => {
-    console.log((data.result.n = ` records inserted!`));
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+
+module.exports = {
+  menuSeed
+};
