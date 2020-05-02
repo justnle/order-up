d8b0d6425a49b8e83b68696f2c4309e4c2f6a579
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-05-02 05:37:53 -0700

    Shift seed

---
+++ seeds/shiftsSeed.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const shiftSeed = [
+  {
+    employeeId: 23456,
+    employeeName: `Zahra Merrill`,
+    clockIn: Date.now(),
+    clockOut: new Date('<2020-05-01T08:27:03>')
+  },
+  {
+    employeeId: 452885,
+    employeeName: `Anish Mcgowan`,
+    clockIn: Date.now(),
+    clockOut: new Date('<2020-05-02T10:05:00>')
+  },
+  {
+    employeeId: 369979,
+    employeeName: `Salahuddin Haas"`,
+    clockIn: Date.now(),
+    clockOut: new Date('<2020-05-02T13:00:01>')
+  },
+  {
+    employeeId: 667475,
+    employeeName: `Fahima Ayers`,
+    clockIn: Date.now(),
+    clockOut: new Date('<2020-05-02T11:58:21>')
+  },
+  {
+    employeeId: 782914,
+    employeeName: `Nadia Huber`,
+    clockIn: Date.now(),
+    clockOut: new Date('<2020-05-02T14:55:04>')
+  }
+];
+module.exports = {
+  shiftSeed
+};
