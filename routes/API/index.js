a1d2ea522817f5a38940a91616700eb346caa085
Richard Wang <richardchacha@hotmail.com>
2020-05-01 18:39:18 -0700

    remove comment from homework

--- routes/API/index.js
+++ routes/API/index.js
@@ -5,7 +5,7 @@ const employeeRoutes = require(`./employee`);
 const inventoryRoutes = require(`./inventory`);
 const menuRoutes = require(`./menu`);
 const timeRoutes = require(`./time`);
-// Book routes
+
 router.use(`/employee`, employeeRoutes);
 router.use(`/inventory`, inventoryRoutes);
 router.use(`/menu`, menuRoutes);
