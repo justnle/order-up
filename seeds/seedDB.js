c80081c7657418dc7ac1eec35ab4baef1531c307
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-05-02 05:41:43 -0700

    Shift seed done

--- seeds/seedDB.js
+++ seeds/seedDB.js
@@ -5,7 +5,7 @@ const { employeeSeed } = require(`./employeeSeed`);
 const { menuSeed } = require(`./menuSeed`);
 const { orderSeed } = require(`./orderSeed`);
 const { archivedOrderSeed } = require(`./archivedOrderSeed`);
-
+const { shiftSeed } = require(`./shiftSeed`);
 const mongoose = require(`mongoose`);
 const db = require(`../models`);
 
@@ -17,26 +17,43 @@ mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/orderup`, {
 async function seedDB() {
   const models = [
     {
-      modelName: db.Inventory, data: inventorySeed
+      modelName: db.Inventory,
+      data: inventorySeed
     },
     {
-      modelName: db.Employee, data: employeeSeed
+      modelName: db.Employee,
+      data: employeeSeed
     },
     {
-      modelName: db.Menu, data: menuSeed
+      modelName: db.Menu,
+      data: menuSeed
     },
     {
-      modelName: db.ActiveOrder, data: orderSeed
+      modelName: db.ActiveOrder,
+      data: orderSeed
     },
     {
-      modelName: db.ArchivedOrder, data: archivedOrderSeed
+      modelName: db.ArchivedOrder,
+      data: archivedOrderSeed
+    },
+    {
+      modelName: db.Time,
+      data: shiftSeed
     }
   ];
   for (const model of models) {
-    await model.modelName.collection.deleteMany({})
-      .then(() => model.modelName.collection.insertMany(model.data)
-        .then(res => console.log(`${res.result.n} ${model.modelName.collection.name} records inserted!`)))
-      .catch(err => {
+    await model.modelName.collection
+      .deleteMany({})
+      .then(() =>
+        model.modelName.collection
+          .insertMany(model.data)
+          .then((res) =>
+            console.log(
+              `${res.result.n} ${model.modelName.collection.name} records inserted!`
+            )
+          )
+      )
+      .catch((err) => {
         console.error(err);
       });
   }
