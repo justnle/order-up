c3db9971bb3d707ce226b2e4278f86ed8361a5f5
Richard Wang <richardchacha@hotmail.com>
2020-05-01 18:45:56 -0700

    update routes index.js

--- routes/API/index.js
+++ routes/API/index.js
@@ -5,10 +5,14 @@ const employeeRoutes = require(`./employee`);
 const inventoryRoutes = require(`./inventory`);
 const menuRoutes = require(`./menu`);
 const timeRoutes = require(`./time`);
+const activeOrderRoutes = require(`./activeOrder`);
+const archivedOrderRoutes = require(`./archivedOrder`);
 
 router.use(`/employee`, employeeRoutes);
 router.use(`/inventory`, inventoryRoutes);
 router.use(`/menu`, menuRoutes);
 router.use(`/time`, timeRoutes);
+router.use(`/activeorder`, activeOrderRoutes);
+router.use(`/archivedorder`, archivedOrderRoutes);
 
 module.exports = router;
