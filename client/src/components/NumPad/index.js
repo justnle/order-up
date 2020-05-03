3c4272367b671eb228baabf8eda16212d2a9a94c
Justin <twoplatinumchainz@gmail.com>
2020-05-03 09:21:47 -0700

    Manager redirect

--- client/src/components/NumPad/index.js
+++ client/src/components/NumPad/index.js
@@ -2,23 +2,31 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Form from 'react-bootstrap/Form';
+import { Redirect } from 'react-router-dom';
+import API from '../../utils/employeesAPI';
 
 export default function NumPad() {
   const [pin, setPin] = useState(``);
+  const [redirect, setRedirect] = useState(false);
 
   const handleBtnInput = (event) => {
     event.preventDefault();
     const { value } = event.target;
     setPin(pin + value);
-  };
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const userData = {
-      pin: pin
-    };
-    console.log(userData);
-  };
+    if (pin.length > 0) {
+      API.getEmployees()
+        .then((res) => {
+          res.data.some((employee) => employee.id === pin)
+            ? setRedirect(true)
+            : errorPin();
+        })
+        .catch((err) => console.error(err));
+    }
+  }
 
   const handleDelete = () => {
     if (pin.length > 0) {
@@ -26,7 +34,12 @@ export default function NumPad() {
       pinStr = pinStr.slice(0, -1);
       setPin(pinStr);
     }
-  };
+  }
+
+  const errorPin = () => {
+    setPin(``);
+    console.log(`incorrect pin`);
+  }
 
   const buttons = [
     [
@@ -78,41 +91,47 @@ export default function NumPad() {
   ];
 
   return (
-    <ButtonGroup vertical>
-      <Form>
-        <Form.Group controlId='formPIN'>
-          <Form.Control
-            autoFocus
-            name='pin'
-            type='password'
-            placeholder='Enter PIN'
-            value={pin}
-            onChange={(e) => {
-              setPin(e.target.value);
-            }}
-          />
-        </Form.Group>
-      </Form>
-      {buttons.map((rows, index) => (
-        <ButtonGroup key={`button-row-${index}`}>
-          {rows.map((button, buttonIndex) => (
-            <Button
-              variant='outline-secondary'
-              key={`button-index-${buttonIndex}`}
-              value={!button.value ? button.name : button.value}
-              onClick={
-                button.value === `del`
-                  ? handleDelete
-                  : button.value === `login`
-                  ? handleSubmit
-                  : handleBtnInput
-              }
-            >
-              {button.name}
-            </Button>
-          ))}
-        </ButtonGroup>
-      ))}
-    </ButtonGroup>
+    <>
+      {redirect ? <Redirect to='/manager' /> : null}
+      <ButtonGroup vertical>
+        <Form>
+          <Form.Group controlId='formPIN'>
+            <Form.Control
+              autoFocus
+              name='pin'
+              type='password'
+              placeholder='Enter PIN'
+              value={pin}
+              onChange={(e) => {
+                setPin(e.target.value);
+              }}
+            />
+          </Form.Group>
+        </Form>
+        {buttons.map((rows, index) => (
+          <ButtonGroup key={`button-row-${index}`}>
+            {rows.map((button, buttonIndex) => (
+              <Button
+                variant='outline-secondary'
+                key={`button-index-${buttonIndex}`}
+                value={!button.value ? button.name : button.value}
+                onClick={
+                  button.value === `del`
+                    ? handleDelete
+                    : button.value === `login`
+                    ? handleSubmit
+                    : handleBtnInput
+                }
+              >
+                {button.name}
+              </Button>
+            ))}
+          </ButtonGroup>
+        ))}
+      </ButtonGroup>
+    </>
   );
 }
+
+
+//TODO:
