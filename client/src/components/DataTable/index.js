ab98333fbc11045c7e9f4b234ed5552453065783
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-05-02 22:55:30 -0700

    Checkbox

--- client/src/components/DataTable/index.js
+++ client/src/components/DataTable/index.js
@@ -1,32 +1,37 @@
-import React from "react";
-import Table from "react-bootstrap/Table";
-import InputGroup from 'react-bootstrap/InputGroup';
-
-const DataTable = props => {
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import Form from 'react-bootstrap/Form';
+const DataTable = (props) => {
   return (
     <Table responsive className='text-white'>
       <thead>
         <tr>
           <th hidden={props.hideEdit}>Select</th>
-          {props.headingArr.map(heading => {
-            return (
-              <th key={heading.key}>{heading.heading}</th>
-            )
+          {props.headingArr.map((heading) => {
+            return <th key={heading.key}>{heading.heading}</th>;
           })}
         </tr>
       </thead>
       <tbody>
-        {props.dataArr.map(data => {
+        {props.dataArr.map((data) => {
           return (
             <tr key={data._id}>
-              <td hidden={props.hideEdit}><InputGroup.Checkbox data-id={data._id} onClick={props.clickCheckbox}/></td>
-              {props.headingArr.map(heading => <td key={`${data._id}${heading.key}`}>{data[heading.key]}</td>)}
+              <td hidden={props.hideEdit}>
+                <Form.Check
+                  data-id={data._id}
+                  onClick={props.clickCheckbox}
+                  className='checkbox'
+                ></Form.Check>
+              </td>
+              {props.headingArr.map((heading) => (
+                <td key={`${data._id}${heading.key}`}>{data[heading.key]}</td>
+              ))}
             </tr>
-          )
+          );
         })}
       </tbody>
     </Table>
-  )
+  );
 };
 
 export default DataTable;
