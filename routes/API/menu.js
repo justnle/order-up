167ad26f8c4d1690a140190200faaa3b271dd895
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-05-04 13:24:05 -0700

    Menu controller, routes, and client api updated for deleteMany and updateMany

--- routes/API/menu.js
+++ routes/API/menu.js
@@ -4,7 +4,8 @@ const router = require(`express`).Router();
 const menuController = require(`../../controllers/menuController`);
 
 router.route(`/`).get(menuController.findAll).post(menuController.create);
-
+router.route(`/deleteMany`).delete(menuController.removeMany);
+router.route(`/updateMany`).put(menuController.updateMany);
 router
   .route(`/:id`)
   .get(menuController.findById)
