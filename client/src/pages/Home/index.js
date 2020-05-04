a83dae932d763aee97231ddafd0c5bf3de8ff8a6
Justin <twoplatinumchainz@gmail.com>
2020-05-04 00:01:14 -0700

    Extracted ClockIn and ClockOut to NumPad

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -5,25 +5,13 @@ import { ClockInButton, ClockOutButton } from '../../components/Buttons';
 
 import './style.css';
 
-function Home() {
+function Home(props) {
   return (
     <Container
       className='border border-dark rounded text-center mt-5'
       id='home-container'
     >
-      <Row id='home-row'>
-        <Col className='py-3'>
-          <NumPad />
-        </Col>
-        <Col className='my-auto clock-btns'>
-          <Row className='justify-content-center py-1'>
-            <ClockInButton />
-          </Row>
-          <Row className='justify-content-center py-1'>
-            <ClockOutButton />
-          </Row>
-        </Col>
-      </Row>
+        <NumPad />
     </Container>
   );
 }
