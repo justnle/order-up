1e0773fb89f88845ff5db30672ef7dc93901fbb8
Richard Wang <richardchacha@hotmail.com>
2020-05-05 00:30:23 -0700

    update menu seed data price

--- seeds/menuSeed.js
+++ seeds/menuSeed.js
@@ -5,7 +5,7 @@ const menuSeed = [
   {
     category: `Food`,
     name: `Charcuterie Board`,
-    price: 18,
+    price: `18.00`,
     description: `Beechers Flagship Cheddar, Prosciutto, Grapes, and Grand Central Bakery Multigrain Bread`,
     pairing: `Pinot Grigio`,
     prepareTime: 10
@@ -13,7 +13,7 @@ const menuSeed = [
   {
     category: `Food`,
     name: `Hand Cut Fries doused in Cheddar Sauce`,
-    price: 6,
+    price: `6.00`,
     description: `Shoestring fries seasoned with salt and pepper with a drizzling of cheddar sauce for extra oomph`,
     pairing: `IPA`,
     prepareTime: 10
@@ -21,7 +21,7 @@ const menuSeed = [
   {
     category: `Food`,
     name: `Oysters 6`,
-    price: 18,
+    price: `18.00`,
     description: `Fresh Hama Hama oysters served with sides of cocktail sauce and mignonette`,
     pairing: `Pilsner`,
     prepareTime: 15
@@ -29,7 +29,7 @@ const menuSeed = [
   {
     category: `Food`,
     name: `Grilled Veggie Sandwich`,
-    price: 12,
+    price: `12.50`,
     description: `Grilled broccoli, carrots, spinach and feta on toasty naan`,
     pairing: `Pilsner`,
     prepareTime: 20
@@ -37,7 +37,7 @@ const menuSeed = [
   {
     category: `Food`,
     name: `The Burger to End all Burgers`,
-    price: 14,
+    price: `14.50`,
     description: `1/2 lb burger on toasted Grand Central Bakery Como bread. Toppings include fried onions, mushrooms, and pepperjack cheese.`,
     pairing: `Amber`,
     prepareTime: 20
@@ -45,7 +45,7 @@ const menuSeed = [
   {
     category: `Food`,
     name: `Roasted Lemon Garlic Chicken`,
-    price: 20,
+    price: `20.99`,
     description: `Served with charred broccoli and roasted baby red potatoes. `,
     pairing: `Pilsner`,
     prepareTime: 25
@@ -53,7 +53,7 @@ const menuSeed = [
   {
     category: `Food`,
     name: `NY Strip Steak Frites`,
-    price: 30,
+    price: `30.00`,
     description: `Served with asparagus, basil butter, garlic aioli and hand cut fries`,
     pairing: `Malbec`,
     prepareTime: 25
@@ -61,7 +61,7 @@ const menuSeed = [
   {
     category: `Beverage`,
     name: `Pinot Grigio`,
-    price: 7,
+    price: `7.99`,
     description: `Dry white wine that has a punchy acidity with flavors of lemons, limes, green apples and honeysuckle`,
     pairing: `Charcuterie`,
     prepareTime: 5
@@ -69,7 +69,7 @@ const menuSeed = [
   {
     category: `Beverage`,
     name: `Malbec`,
-    price: 7,
+    price: `7.99`,
     description: `Dark, inky purple color profiles and ripe flavors of plums, black cherry, and blackberry can give this wine a decidedly jammy character`,
     pairing: `NY Strip Steak Frites`,
     prepareTime: 5
@@ -77,7 +77,7 @@ const menuSeed = [
   {
     category: `Beverage`,
     name: `Pilsner`,
-    price: 6,
+    price: `6.99`,
     description: `Light straw color with spicy floral and strong hop flavors`,
     pairing: `Roasted Lemon Garlic Chicken`,
     prepareTime: 5
@@ -85,7 +85,7 @@ const menuSeed = [
   {
     category: `Beverage`,
     name: `Amber`,
-    price: 6,
+    price: `6.99`,
     description: `Quite malty but not heavily caramelized in flavor`,
     pairing: `The Burger to End All Burgers`,
     prepareTime: 5
@@ -93,7 +93,7 @@ const menuSeed = [
   {
     category: `Beverage`,
     name: `IPA`,
-    price: 6,
+    price: `6.99`,
     description: `Bitter, floral, earthy, citrusy, piney, fruity`,
     pairing: `Hand Cut Fries`,
     prepareTime: 5
