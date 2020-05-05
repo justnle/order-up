dd9359c17b6d18ec02634d7c544a0f5984957043
Justin <twoplatinumchainz@gmail.com>
2020-05-04 18:47:31 -0700

    added onTheClock property

--- seeds/shiftSeed.js
+++ seeds/shiftSeed.js
@@ -4,27 +4,32 @@ const shiftSeed = [
   {
     employeeId: 123456,
     employeeName: `Zahra Merrill`,
-    clockIn: Date.now()
+    clockIn: Date.now(),
+    onTheClock: null
   },
   {
     employeeId: 452885,
     employeeName: `Anish Mcgowan`,
-    clockIn: Date.now()
+    clockIn: Date.now(),
+    onTheClock: null
   },
   {
     employeeId: 369979,
     employeeName: `Salahuddin Haas"`,
-    clockIn: Date.now()
+    clockIn: Date.now(),
+    onTheClock: null
   },
   {
     employeeId: 667475,
     employeeName: `Fahima Ayers`,
-    clockIn: Date.now()
+    clockIn: Date.now(),
+    onTheClock: null
   },
   {
     employeeId: 782914,
     employeeName: `Nadia Huber`,
-    clockIn: Date.now()
+    clockIn: Date.now(),
+    onTheClock: null
   }
 ];
 module.exports = {
