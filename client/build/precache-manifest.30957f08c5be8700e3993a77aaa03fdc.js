193411b8fdc177aea2257dc8c0c90073652a9e1c
Justin <twoplatinumchainz@gmail.com>
2020-05-04 19:02:10 -0700

    appliation build

---
+++ client/build/precache-manifest.30957f08c5be8700e3993a77aaa03fdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddcb05011387bc7f0b2bfc18f520d463",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ba70f2fed40a0c7dc11",
+    "url": "/static/css/main.61ac4ff1.chunk.css"
+  },
+  {
+    "revision": "acf79352075ef4b111f2",
+    "url": "/static/js/2.dfd8ffbd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dfd8ffbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba70f2fed40a0c7dc11",
+    "url": "/static/js/main.12480673.chunk.js"
+  },
+  {
+    "revision": "6a0401fd6432a83d40ce",
+    "url": "/static/js/runtime-main.7a365a2e.js"
+  }
+]);
