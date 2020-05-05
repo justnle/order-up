0001a9883c5642559f7a85a2c6a8875c43223a85
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-05-05 10:31:42 -0700

    Vendor seed set up

--- seeds/seedDB.js
+++ seeds/seedDB.js
@@ -6,6 +6,7 @@ const { menuSeed } = require(`./menuSeed`);
 const { orderSeed } = require(`./orderSeed`);
 const { archivedOrderSeed } = require(`./archivedOrderSeed`);
 const { shiftSeed } = require(`./shiftSeed`);
+const { vendorSeed } = require(`./vendorSeed`);
 const mongoose = require(`mongoose`);
 const db = require(`../models`);
 
@@ -39,6 +40,10 @@ async function seedDB() {
     {
       modelName: db.Time,
       data: shiftSeed
+    },
+    {
+      modelName: db.Vendor,
+      data: vendorSeed
     }
   ];
   for (const model of models) {
---
+++ seeds/vendorSeed.js
@@ -0,0 +1,32 @@
+/* eslint-disable max-len */
+'use strict';
+
+const vendorSeed = [
+  {
+    vendorName: `Charlie's Produce`,
+    vendorContactName: `Tom`,
+    vendorPhoneNumber: `206-625-1412`,
+    vendorEmail: `charliesproduce@gmail.com`
+  },
+  {
+    vendorName: `La Mexicana`,
+    vendorContactName: `José Cifuentes`,
+    vendorPhoneNumber: `206-625-1412`,
+    vendorEmail: `josecifuen@gmail.com`
+  },
+  {
+    vendorName: `Grand Central Bakery`,
+    vendorContactName: `Gabrielle Moorhead`,
+    vendorPhoneNumber: `206-768-0320`,
+    vendorEmail: `gabby@grandcentralbakery.com`
+  },
+  {
+    vendorName: `Merlino Foods`,
+    vendorContactName: `Jennifer Markham`,
+    vendorPhoneNumber: `206-723-4700`,
+    vendorEmail: `sales@merlino.com`
+  }
+];
+module.exports = {
+  vendorSeed
+};
