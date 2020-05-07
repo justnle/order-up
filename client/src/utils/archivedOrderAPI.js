f75a0e4ae8751981747158281edcce41591975c1
Justin <twoplatinumchainz@gmail.com>
2020-05-07 02:54:16 -0700

    fixed api routes

--- client/src/utils/archivedOrderAPI.js
+++ client/src/utils/archivedOrderAPI.js
@@ -2,22 +2,23 @@ import axios from 'axios';
 
 export default {
   getArchivedOrders: function () {
-    return axios.get(`/api/archivedorders`);
+    return axios.get(`/api/archivedorder`);
   },
 
   getArchivedOrder: function (id) {
-    return axios.get(`/api/archivedorders/${id}`);
+    return axios.get(`/api/archivedorder/${id}`);
   },
 
   addArchivedOrder: function (archivedData) {
-    return axios.post(`/api/archivedorders`, archivedData);
+    console.log(archivedData);
+    return axios.post(`/api/archivedorder`, archivedData);
   },
 
   updateArchivedOrder: function (id, data) {
-    return axios.put(`/api/archivedorders/${id}`, data);
+    return axios.put(`/api/archivedorder/${id}`, data);
   },
 
   deleteArchivedOrder: function (id) {
-    return axios.delete(`/api/archivedorders/${id}`);
+    return axios.delete(`/api/archivedorder/${id}`);
   }
 };
