a916a2fcdd4e75394918f94afc6ad079682f748f
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-05-07 16:34:47 -0700

    added delete button for removing items from seat order

--- client/src/components/SeatOrder/index.js
+++ client/src/components/SeatOrder/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import FormCheck from 'react-bootstrap/FormCheck'
 import './style.css';
-
 
 function SeatOrder(props) {
   return (
@@ -11,7 +8,18 @@ function SeatOrder(props) {
       <td>
         <ul>
           {props.seatOrder.map((orderItem, index) => (
-            <li key={orderItem._id + '_' + index}>{orderItem.name} <Form.Check inline onClick={props.clickCheckbox} data-id={orderItem._id}/></li>
+            <li key={orderItem._id + '_' + index}>
+              {orderItem.name}
+              <span
+                className='delete-btn'
+                onClick={props.clickDeleteBtn}
+                role='button'
+                tabIndex='0'
+                data-id={index}
+              >
+                ✗
+              </span>
+            </li>
           ))}
         </ul>
       </td>
