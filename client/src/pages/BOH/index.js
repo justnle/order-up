d422e12cbc71942448121c6b8ba9e2ad8f5bfc77
Justin <twoplatinumchainz@gmail.com>
2020-05-07 03:40:04 -0700

    adding/deleting orders

--- client/src/pages/BOH/index.js
+++ client/src/pages/BOH/index.js
@@ -9,78 +9,119 @@ import Button from 'react-bootstrap/Button';
 
 function Boh() {
   const [activeOrders, setActiveOrders] = useState();
-  const [archivedOrders, setArchivedOrders] = useState();
+  const [archivedOrder, setArchivedOrder] = useState([]);
 
   useEffect(() => {
     loadOrders();
-  }, []);
+  }, [activeOrders]);
+
+  useEffect(() => {
+    if (archivedOrder.length > 0) {
+      addArchived();
+    }
+  }, [archivedOrder]);
 
   const loadOrders = () => {
     ACTIVE_ORDER_API.getActiveOrders()
       .then((res) => {
-        console.log(res.data);
-        setActiveOrders(res.data);
+        if (res.data.length > 0) {
+          setActiveOrders(res.data);
+        }
       })
       .catch((err) => console.error(err));
   };
 
-  const archiveOrder = () => {
-    ARCHIVED_ORDER_API.addArchivedOrder()
-      .then((res) => {
-        setArchivedOrders();
-      })
-      .catch((err) => console.error(err));
-  };
-
-  const test = (event) => {
+  const removeActive = (event) => {
     event.preventDefault();
     const { value } = event.target;
-    console.log(value);
-  }
+
+    ACTIVE_ORDER_API.getActiveOrder(value)
+      .then((res) => {
+        setArchivedOrder([res.data]);
+      })
+      .catch((err) => console.error(err));
+
+    ACTIVE_ORDER_API.deleteActiveOrder(value)
+      .then((res) => {
+        if (res.data.length > 0) {
+          loadOrders();
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const addArchived = () => {
+    ARCHIVED_ORDER_API.addArchivedOrder(archivedOrder).catch((err) =>
+      console.error(err)
+    );
+  };
 
   return (
     <Container>
       <BohHeader />
-      {activeOrders
-        ? activeOrders.map((data, index) => (
-            <Row className='py-2 border' key={`table-row-${index + 1}`}>
-              <Col md={2} className='my-auto text-center' key={`table-col-${index + 1}`}>
-                <span>Table #{data.tableNumber}</span>
+      {activeOrders ? (
+        activeOrders.map((data, index) => (
+          <Row className='py-2 border' key={`table-row-${index + 1}`}>
+            <Col
+              md={2}
+              className='my-auto text-center'
+              key={`table-col-${index + 1}`}
+            >
+              <span>Table #{data.tableNumber}</span>
+            </Col>
+            <Col
+              md={6}
+              className='d-flex flex-row my-auto'
+              key={`order-col-${index + 1}`}
+            >
+              <Col key={`order-items-col-${index + 1}`}>
+                {data.seatOrders.map((order, index) => (
+                  <Row
+                    className='d-flex flex-column'
+                    key={`order-item-${index + 1}`}
+                  >
+                    {order.menuItems.map((items, index) => (
+                      <Row
+                        className='text-center'
+                        key={`menu-items-${index + 1}`}
+                      >
+                        <Col className='text-center my-auto' md={1}>
+                          {order.seatNumber}
+                        </Col>
+                        <Col>{items.itemName}</Col>
+                      </Row>
+                    ))}
+                  </Row>
+                ))}
               </Col>
-              <Col md={6} className='d-flex flex-row my-auto' key={`order-col-${index + 1}`}>
-                <Col key={`order-items-col-${index + 1}`}>
-                  {data.seatOrders.map((order, index) => (
-                    <Row
-                      className='d-flex flex-column'
-                      key={`order-item-${index + 1}`}
-                    >
-                      {order.menuItems.map((items, index) => (
-                        <Row
-                          className='text-center'
-                          key={`menu-items-${index + 1}`}
-                        >
-                          <Col className='text-center my-auto' md={1}>
-                            {order.seatNumber}
-                          </Col>
-                          <Col>{items.itemName}</Col>
-                        </Row>
-                      ))}
-                    </Row>
-                  ))}
-                </Col>
-              </Col>
-              <Col md={2} className='text-center my-auto' key={`time-col-${index + 1}`}>
-                {format(Date.parse(data.orderInTime), 'pp')}
-              </Col>
-              <Col md={2} className='text-center my-auto' key={`sold-col-${index + 1}`}>
-                <Button variant='success' value={data._id} onClick={test}>CHECK</Button>
-              </Col>
-            </Row>
-          ))
-        : console.log(`nothing there`)}
+            </Col>
+            <Col
+              md={2}
+              className='text-center my-auto'
+              key={`time-col-${index + 1}`}
+            >
+              {format(Date.parse(data.orderInTime), 'pp')}
+            </Col>
+            <Col
+              md={2}
+              className='text-center my-auto'
+              key={`sold-col-${index + 1}`}
+            >
+              <Button variant='success' value={data._id} onClick={removeActive}>
+                CHECK
+              </Button>
+            </Col>
+          </Row>
+        ))
+      ) : (
+        <Container fluid className='h-100'>
+          <Row className='justify-content-center h-100'>
+            <h1 className='my-auto'>No Active Orders</h1>
+          </Row>
+        </Container>
+      )}
     </Container>
   );
 }
 
 export default Boh;
-git 
