a24838b170c47a586c3e0af8c94396e01c69f435
Justin <twoplatinumchainz@gmail.com>
2020-05-06 17:35:34 -0700

    vertical align, some fx WIP

--- client/src/pages/BOH/index.js
+++ client/src/pages/BOH/index.js
@@ -13,15 +13,29 @@ function Boh() {
 
   useEffect(() => {
     loadOrders();
-    console.log(activeOrders);
   }, []);
 
   const loadOrders = () => {
-    ACTIVE_ORDER_API.getActiveOrders().then((res) => {
-      console.log(res.data);
-      setActiveOrders(res.data);
-    });
+    ACTIVE_ORDER_API.getActiveOrders()
+      .then((res) => {
+        setActiveOrders(res.data);
+      })
+      .catch((err) => console.error(err));
   };
+
+  const archiveOrder = () => {
+    ARCHIVED_ORDER_API.addArchivedOrder()
+      .then((res) => {
+        setArchivedOrders();
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const test = (event) => {
+    event.preventDefault();
+    const { value } = event.target;
+    console.log(value);
+  }
 
   return (
     <Container>
@@ -29,19 +43,24 @@ function Boh() {
       {activeOrders
         ? activeOrders.map((data, index) => (
             <Row className='py-2 border' key={`table-row-${index + 1}`}>
-              <Col md={2} className='my-auto' key={`table-col`}>
-                <h6>Table #{data.tableNumber}</h6>
+              <Col md={2} className='my-auto text-center' key={`table-col-${index + 1}`}>
+                <span>Table #{data.tableNumber}</span>
               </Col>
-              <Col md={6} className='d-flex flex-row' key={`order-col`}>
-                <Col key={`order-items-col`}>
+              <Col md={6} className='d-flex flex-row my-auto' key={`order-col-${index + 1}`}>
+                <Col key={`order-items-col-${index + 1}`}>
                   {data.seatOrders.map((order, index) => (
                     <Row
                       className='d-flex flex-column'
                       key={`order-item-${index + 1}`}
                     >
                       {order.menuItems.map((items, index) => (
-                        <Row className='text-center' key={`menu-items-${index + 1}`}>
-                          <Col className='text-center' md={1}>{order.seatNumber}</Col>
+                        <Row
+                          className='text-center'
+                          key={`menu-items-${index + 1}`}
+                        >
+                          <Col className='text-center my-auto' md={1}>
+                            {order.seatNumber}
+                          </Col>
                           <Col>{items.itemName}</Col>
                         </Row>
                       ))}
@@ -49,11 +68,11 @@ function Boh() {
                   ))}
                 </Col>
               </Col>
-              <Col md={2} className='text-center my-auto' key={`time-col`}>
+              <Col md={2} className='text-center my-auto' key={`time-col-${index + 1}`}>
                 {format(Date.parse(data.orderInTime), 'pp')}
               </Col>
-              <Col md={2} className='text-center my-auto' key={`sold-col`}>
-                <Button variant='success'>CHECK</Button>
+              <Col md={2} className='text-center my-auto' key={`sold-col-${index + 1}`}>
+                <Button variant='success' onClick={test}>CHECK</Button>
               </Col>
             </Row>
           ))
