9191859194424e12f26f17fce536bfda0808b8fd
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-05-07 17:59:53 -0700

    removed commented out code and updated background color to dark

--- client/src/components/MenuItem/index.js
+++ client/src/components/MenuItem/index.js
@@ -5,15 +5,14 @@ import './style.css';
 
 function MenuItem(props) {
   return (
-    <div className= 'd-flex text-center'>
+    <div className= 'd-flex text-center bg-dark'>
       <Card
         id='menuItem'
         value={
           !props.menuItem.value ? props.menuItem.name : props.menuItem.value
         }
         style={{ width: '11rem' }}
-        bg='transparent'
-        className='text-white'
+        className='bg-dark text-white'
       >
         <Card.Body>
           <Card.Text>{props.menuItem.name}</Card.Text>
@@ -35,44 +34,6 @@ function MenuItem(props) {
             </Button>
         </Card.Body>
       </Card>
-
-      {/* <div
-        className='m-1 text-white'
-        id='menuItem'
-        value={
-          !props.menuItem.value ? props.menuItem.name : props.menuItem.value
-        }
-        style={{
-          width: '8em',
-          height: '8em',
-          backgroundColor: 'transparent',
-          border: 'solid 1px #6c757d',
-          borderRadius: '4px',
-          textAlign: 'center',
-        }}
-      >
-        {props.menuItem.name}
-        <div className='d-flex justify-content-center align-content-flex-end mt-3'>
-          <Row>
-            <Button
-              className='mx-1'
-              size='sm'
-              variant='primary'
-              onClick={props.handleAddToSeatOrder}
-            >
-              Select
-            </Button>
-            <Button
-              className='mx-1'
-              size='sm'
-              variant='success'
-              onClick={props.handleShow}
-            >
-              View
-            </Button>
-          </Row>
-        </div>
-      </div> */}
     </div>
   );
 }
