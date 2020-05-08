16f064b4f0032ac74ebd2a8bbfc520241a29935b
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-05-07 21:08:36 -0700

    Working through the console warnings

--- client/src/components/FloorPlan/index.js
+++ client/src/components/FloorPlan/index.js
@@ -1,111 +1,122 @@
 import React from 'react';
 import './style.css';
 class Seats extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      seat: ['#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8', '#9'],
+      seatAvailable: ['#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8', '#9'],
+      seatReserved: []
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            seat: [
-                '#1', '#2', '#3',
-                '#4', '#5', '#6',
-                '#7', '#8', '#9'
-            ],
-            seatAvailable: [
-                '#1', '#2', '#3',
-                '#4', '#5', '#6',
-                '#7', '#8', '#9'
-            ],
-            seatReserved: []
-        }
+  onClickData(seat) {
+    if (this.state.seatReserved.indexOf(seat) > -1) {
+      this.setState({
+        seatAvailable: this.state.seatAvailable.concat(seat),
+        seatReserved: this.state.seatReserved.filter((res) => res !== seat)
+      });
+    } else {
+      this.setState({
+        seatReserved: this.state.seatReserved.concat(seat),
+        seatAvailable: this.state.seatAvailable.filter((res) => res !== seat)
+      });
     }
+  }
 
-    onClickData(seat) {
-        if (this.state.seatReserved.indexOf(seat) > -1) {
-            this.setState({
-                seatAvailable: this.state.seatAvailable.concat(seat),
-                seatReserved: this.state.seatReserved.filter(res => res != seat)
-            })
-        } else {
-            this.setState({
-                seatReserved: this.state.seatReserved.concat(seat),
-                seatAvailable: this.state.seatAvailable.filter(res => res != seat)
-            })
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <DrawGrid
-                    seat={this.state.seat}
-                    available={this.state.seatAvailable}
-                    reserved={this.state.seatReserved}
-                    onClickData={this.onClickData.bind(this)}
-                />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <DrawGrid
+          seat={this.state.seat}
+          available={this.state.seatAvailable}
+          reserved={this.state.seatReserved}
+          onClickData={this.onClickData.bind(this)}
+        />
+      </div>
+    );
+  }
 }
 
 class DrawGrid extends React.Component {
-    render() {
-        return (
-            <div id="seatsContainer"className='container'>
-                <h2></h2>
-                <table className='grid'>
-                    <tbody>
-                        <tr>
-                            {this.props.seat.map(row =>
-                                <td
-                                    className={this.props.reserved.indexOf(row) > -1 ? 'reserved' : 'available'}
-                                    key={row} onClick={e => this.onClickSeat(row)}>{row} </td>)}
-                        </tr>
-                    </tbody>
-                </table>
-                <div className='row'>
-                    <div className="col-sm">
-                        <AvailableList available={this.props.available} />
-                    </div>
-                    <div className="col-sm">
-                        <ReservedList reserved={this.props.reserved} />
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div
+        id='seatsContainer'
+        className='container d-flex row justify-content-center '
+      >
+        <h2 className='display-4 d-flex justify-content-center mt-5 mb-5'>
+          Floor Plan
+        </h2>
+        <table className='grid '>
+          <tbody>
+            <tr>
+              {this.props.seat.map((row) => (
+                <td
+                  className={
+                    this.props.reserved.indexOf(row) > -1
+                      ? 'reserved'
+                      : 'available'
+                  }
+                  key={row}
+                  onClick={(e) => this.onClickSeat(row)}
+                >
+                  {row}{' '}
+                </td>
+              ))}
+            </tr>
+          </tbody>
+        </table>
 
-    onClickSeat(seat) {
-        this.props.onClickData(seat);
-    }
+        <div className='row mt-5'>
+          <div className='col-sm'>
+            <AvailableList available={this.props.available} />
+          </div>
+          <div className='col-sm'>
+            <ReservedList reserved={this.props.reserved} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  onClickSeat(seat) {
+    this.props.onClickData(seat);
+  }
 }
 
 class AvailableList extends React.Component {
-    render() {
-        const seatCount = this.props.available.length;
-        return (
-            <div className='left'>
-                <h4>Available Seats: ({seatCount == 0 ? 'No seats available' : seatCount})</h4>
-                <ul>
-                    {this.props.available.map(res => <li key={res} >{res}</li>)}
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    const seatCount = this.props.available.length;
+    return (
+      <div className='left'>
+        <h4>
+          Available Seats: ({seatCount === 0 ? 'No seats available' : seatCount}
+          )
+        </h4>
+        <ul>
+          {this.props.available.map((res) => (
+            <li key={res}>{res}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 class ReservedList extends React.Component {
-    render() {
-        return (
-            <div className='right'>
-                <h4>Reserved Seats: ({this.props.reserved.length})</h4>
-                <ul>
-                    {this.props.reserved.map(res => <li key={res} >{res}</li>)}
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='right'>
+        <h4>Reserved Seats: ({this.props.reserved.length})</h4>
+        <ul>
+          {this.props.reserved.map((res) => (
+            <li key={res}>{res}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-
 export { Seats };
-
