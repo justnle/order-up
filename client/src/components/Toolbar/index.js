91ad646dde685602ff5276391c8d31d299c922c7
Richard Wang <richardchacha@hotmail.com>
2020-05-19 21:52:03 -0700

    add hamburger button

--- client/src/components/Toolbar/index.js
+++ client/src/components/Toolbar/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import DrawerToggleButton from '../DrawerToggleButton';
 import './style.css';
 
 const Toolbar = props => {
   return (
     <header className="toolbar">
       <nav className="toolbar-navigation">
+        <div><DrawerToggleButton /></div>
         <div className="toolbar-logo">Logo</div>
         <div className="spacer" />
         <div className="toolbar-nav-items">
