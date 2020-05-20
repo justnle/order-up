ad2c07e695cc1f7b2542078b9268990a81db3b8f
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-05-19 22:52:37 -0700

    created floorplan seed data

---
+++ seeds/floorPlanSeed.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const floorPlanSeed = [
+  {
+    tableNumber: 1,
+    available: true
+  },
+  {
+    tableNumber: 2,
+    available: true
+  },
+  {
+    tableNumber: 3,
+    available: true
+  },
+  {
+    tableNumber: 4,
+    available: true
+  },
+  {
+    tableNumber: 5,
+    available: true
+  },
+  {
+    tableNumber: 6,
+    available: true
+  },
+  {
+    tableNumber: 7,
+    available: true
+  },
+  {
+    tableNumber: 8,
+    available: true
+  },
+  {
+    tableNumber: 9,
+    available: true
+  }
+];
+
+module.exports = {
+  floorPlanSeed
+};
