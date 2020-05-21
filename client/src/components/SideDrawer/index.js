7647db18863c18fbac04fb7d3341916b1604da88
Richard Wang <richardchacha@hotmail.com>
2020-05-20 19:34:12 -0700

    add icons

--- client/src/components/SideDrawer/index.js
+++ client/src/components/SideDrawer/index.js
@@ -13,13 +13,13 @@ const SideDrawer = props => {
     <nav className={drawerClass}>
       <ul>
         <li><Link to='/'>Home</Link></li>
-        <li><Link to='/menu'>Menu</Link></li>
-        <li><Link to="/employees">Employees</Link></li>
-        <li><Link to="/inventory">Inventory</Link></li>
-        <li><Link to="/shifts">Shifts</Link></li>
-        <li><Link to="/foh">Front</Link></li>
-        <li><Link to="/boh">Back</Link></li>
-        <li><Link to="floorplans">Floor</Link></li>
+        <li><Link to='/menu'><i className='fas fa-book-open fa-1x fa-fw' /><span>Menu</span></Link></li>
+        <li><Link to="/employees"><i className='fas fa-user-friends fa-1x fa-fw' />Employees</Link></li>
+        <li><Link to="/inventory"><i className='fas fa-cubes fa-1x fa-fw' />Inventory</Link></li>
+        <li><Link to="/shifts"><i className='fas fa-clock fa-1x fa-fw' /><span>Shifts</span></Link></li>
+        <li><Link to="/foh"><i className='fas fa-user-tie fa-1x fa-fw' /><span>Front</span></Link></li>
+        <li><Link to="/boh"><i className='fas fa-utensils fa-1x fa-fw' /><span>Back</span></Link></li>
+        <li><Link to="floorplans"><i className='fas fa-chair fa-1x fa-fw' /><span>Floor</span></Link></li>
       </ul>
     </nav>
   )
