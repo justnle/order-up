4fcc2a934929352ddda743b12c2183db7e970a80
Justin <me@justinle.page>
2020-06-04 15:00:19 -0700

    changed 'Logo' to 'Order Up'

--- client/src/components/Toolbar/index.js
+++ client/src/components/Toolbar/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import DrawerToggleButton from '../DrawerToggleButton';
 import './style.css';
 
-const Toolbar = props => {
-  return (
-    <header className="toolbar">
-      <nav className="toolbar-navigation">
-        <div><DrawerToggleButton click={props.clickDrawerButton} /></div>
-        <div className="toolbar-logo">Logo</div>
-        {/* <div className="spacer" /> */}
-        {/* <div className="toolbar-nav-items"></div> */}
-      </nav>
-    </header>
-  )
-}
+const Toolbar = (props) => {
+    return (
+        <header className="toolbar">
+            <nav className="toolbar-navigation">
+                <div>
+                    <DrawerToggleButton click={props.clickDrawerButton} />
+                </div>
+                <div className="toolbar-logo">Order Up</div>
+                {/* <div className="spacer" /> */}
+                {/* <div className="toolbar-nav-items"></div> */}
+            </nav>
+        </header>
+    );
+};
 
 export default Toolbar;
